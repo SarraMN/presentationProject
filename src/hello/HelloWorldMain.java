10a414e4aef9d950b52b7a5c8f6e0c3a72789c7f
SarraMN <sarahmannnai2000@gmail.com>
2023-03-08 10:17:13 +0100

    first commit

---
+++ src/hello/HelloWorldMain.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class HelloWorldMain {
+	public static void main(String[] arg){
+		for (int i = 0; i < 3; i++) {
+			System.out.println("Hello world !!!");
+		}
+		
+	}
+
+}
